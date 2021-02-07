3145a026f27d590228353de7bf83a4d4f2a89caa
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-02-07 20:31:34 +0700

    feat: Add basic DTW implementation

---
+++ program/src/dtw.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <stdexcept>
+
+/**
+ * Calculate the euclidean distance between two points in
+ * n-dimensional space.
+ */
+double calculateCost(std::vector<int> &a, std::vector<int> &b) {
+    if (a.size() != b.size()) {
+        throw std::length_error("Trajectories must reside in the same dimensional space.");
+    }
+
+    double distance = 0;
+
+    for (int it = 0; it < (int)a.size(); it++) {
+        distance += pow(a[it] - b[it], 2);
+    }
+
+    return sqrt(distance);
+}
+
+/**
+ * Calculate dynamic time warping distance between two distinct
+ * trajectories.
+ */
+double calculateDTWDistance(std::vector<std::vector<int> > &a, std::vector<std::vector<int> > &b) {
+    // create a 2-dimensional array to store the DTW distances
+    double dtw[a.size()][b.size()];
+
+    // fill the DTW array with infinities
+    for (int i = 0; i < (int)a.size(); i++) {
+        for (int j = 0; j < (int)b.size(); j++) {
+            dtw[i][j] = std::numeric_limits<int>::max();
+        }
+    }
+
+    // Pair the first point of `a` with the first point of `b`
+    dtw[0][0] = 0;
+
+    for (int i = 1; i < (int)a.size(); i++) {
+        for (int j = 1; j < (int)b.size(); j++) {
+            double cost = calculateCost(a[i], b[i]);
+
+            dtw[i][j] = cost + std::min(
+                dtw[i - 1][j],
+                std::min(
+                    dtw[i][j - 1],
+                    dtw[i - 1][j - 1]
+                )
+            );
+        }
+    }
+
+    return dtw[a.size()][b.size()];
+}
---
+++ program/src/dtw.h
@@ -0,0 +1,8 @@
+#ifndef DTW
+#define DTW
+
+#include <vector>
+
+double calculateDTWDistance(std::vector<std::vector<int> > &a, std::vector<std::vector<int> > &b);
+
+#endif
