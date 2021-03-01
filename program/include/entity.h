451904e974c8ea19888834ce3048b6a7d2960741
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-03-01 10:40:59 +0700

    refactor: Remove operator overloading from entity as it's unused

--- program/include/entity.h
+++ program/include/entity.h
@@ -7,14 +7,6 @@
 struct Entity {
     int id;
     std::map<double, std::vector<double> > trajectories;
-
-    bool operator<(const Entity &o) const {
-        return id < o.id;
-    }
-
-    bool operator==(const Entity &o) const {
-        return id == o.id;
-    }
 };
 
 #endif
